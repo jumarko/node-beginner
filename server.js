7de957fd7cf427fe558c114baf6e2903afedb6a1
Juraj Martinka <juraj@snapshot.travel>
2016-04-17 22:42:44 +0200

    Add a very basic router

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ var url = require("url");
 
 var port = 8888;
 
-function start() {
+function start(route) {
 
     function onRequest(request, response) {
         // this will be printed twice, since most browsers will try to load
         // http://localhost:{port}/favicon.ico
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
+
+        route(pathname);
+
         response.writeHead(200, {"Content-Type" : "text/plain"});
         response.write("Hello World");
         response.end();
