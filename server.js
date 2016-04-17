e9471222f262acca0da44523eb810822620d2554
Juraj Martinka <juraj@snapshot.travel>
2016-04-17 22:35:17 +0200

    Simple server module with logging which pathname has been called.

---
+++ server.js
@@ -0,0 +1,23 @@
+var http = require("http");
+var url = require("url");
+
+var port = 8888;
+
+function start() {
+
+    function onRequest(request, response) {
+        // this will be printed twice, since most browsers will try to load
+        // http://localhost:{port}/favicon.ico
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+        response.writeHead(200, {"Content-Type" : "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(port);
+
+    console.log("Server has started on port " + port);
+}
+
+exports.start = start;
