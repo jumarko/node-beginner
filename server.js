af2493e1b57160c3a20e3392ddf323890e3186a0
Juraj Martinka <juraj@snapshot.travel>
2016-04-18 00:10:58 +0200

    Asynchronous request handlers

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ function start(route, handle) {
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
-        response.writeHead(200, {"Content-Type" : "text/plain"});
-        var  responseBody = route(handle, pathname);
-        response.write(responseBody);
-        response.end();
+        route(handle, pathname, response);
     }
 
     http.createServer(onRequest).listen(port);
