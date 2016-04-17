1977e5d4349f84e47def590c711a0b5887168900
Juraj Martinka <juraj@snapshot.travel>
2016-04-17 23:53:52 +0200

    Demostration of limitations of naive request handling - sleep and blocking.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,13 @@
 function start() {
     console.log("Request handler 'start' was called.");
+
+    function sleep(millis) {
+        var startTime = new Date().getTime();
+        while (new Date().getTime() < startTime + millis);
+    }
+
+    sleep(10000);
+
     return "Hello Start";
 }
 
