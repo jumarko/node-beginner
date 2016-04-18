788cee2fc5ae57dcd5d416eaaa2936e09634b9f7
Juraj Martinka <juraj@snapshot.travel>
2016-04-18 09:28:48 +0200

    Adds input text area for sending data, data is collected in server.js and sent to request handlers once complete

--- server.js
+++ server.js
@@ -8,10 +8,28 @@ function start(route, handle) {
     function onRequest(request, response) {
         // this will be printed twice, since most browsers will try to load
         // http://localhost:{port}/favicon.ico
+
+        var postData = "";
+
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
-        route(handle, pathname, response);
+        // we expect received data to be encoded in UTF-8
+        request.setEncoding("UTF-8");
+
+        // generic listener for collecting all data sent by post
+        // they are received in small chunks, thus we want to collect
+        // them all before sending to request handler
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '" + postDataChunk + "'.");
+        });
+
+        // once all request data has been sent the "end" listener is called,
+        // in which we can forward the data to request handler
+        request.addListener("end", function() {
+            route(handle, pathname, response, postData);
+        });
     }
 
     http.createServer(onRequest).listen(port);
